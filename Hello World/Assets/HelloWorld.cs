ab6f4fb7a1cd04af7055f578b13079105515673a
faizatulfadilah <dhela.faizah@gmail.com>
2021-02-26 06:01:23 +0700

    add helloworld

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld: MonoBehaviour
+{
+    public string myText;
+    // Start is called before the first frame update
+    void Start(){
+        // Debug.Log ("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI(){
+       GUILayout.BeginArea(new React(10,10,200,200));
+       GUILayout.Label(myText);
+       GUILayout.EndArea();
+    }
+}
